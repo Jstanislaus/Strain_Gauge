f1ac465057c029a970802dda5b638f43454e0125
Jstanislaus <48177467+Jstanislaus@users.noreply.github.com>
2025-04-15 16:51:42 +0100

    initial commit

---
+++ Strainguage.py
@@ -0,0 +1,29 @@
+import time
+import RPi.GPIO as GPIO
+from hx711 import HX711
+
+# Set up GPIO
+GPIO.setmode(GPIO.BCM)
+
+# Create an instance of HX711 with GPIO pins for data and clock
+hx = HX711(dout=5, pd_sck=6)
+
+# Set up calibration factor (you might need to experiment with this value)
+hx.set_reading_format("MSB", "MSB")
+hx.set_reference_unit(1)
+
+# Initialize the HX711
+hx.reset()
+hx.tare()
+
+print("Start reading values from strain gauge")
+
+# Continuously read values from the strain gauge
+while True:
+    try:
+        weight = hx.get_weight(5)  # Take 5 readings and average
+        print(f"Weight: {weight} grams")
+        time.sleep(0.1)
+    except (KeyboardInterrupt, SystemExit):
+        GPIO.cleanup()
+        break
